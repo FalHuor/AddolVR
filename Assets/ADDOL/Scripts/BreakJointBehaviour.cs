9819a11f0b78c48e55f0ce9de660eb183b01aae7
CorentinMlo <corentin.millio@viacesi.fr>
2021-01-05 12:32:14 +0100

    ReplacePump

---
+++ Assets/ADDOL/Scripts/BreakJointBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class BreakJointBehaviour : MonoBehaviourPunCallbacks
+{
+
+    public GameObject TriggerZone;
+
+    [PunRPC]
+    private void OnJointBreak(float breakForce)
+    {
+        Debug.Log("Joint Break");
+        TriggerZone.GetComponent<BoxCollider>().enabled = true;
+    }
+}
---
+++ Assets/ADDOL/Scripts/ReplacePump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class ReplacePump : MonoBehaviourPunCallbacks
+{
+    public Transform OldPump;
+    private Transform transformPump;
+    public GameObject Pump;
+    public float time;
+
+    // Start is called before the first frame update
+    [PunRPC]
+    void Start()
+    {
+        transformPump = OldPump;
+    }
+
+    [PunRPC]
+    private void OnTriggerEnter(Collider other)
+    {
+        Quaternion startRotation = Pump.transform.rotation;
+        Vector3 startPosition = Pump.transform.position;
+        if (other.gameObject.name == Pump.name)
+        {
+            for (float t = 0; t < time; t += Time.deltaTime)
+            {
+                Pump.transform.rotation = Quaternion.Lerp(startRotation, transformPump.rotation, Mathf.SmoothStep(0.0f, 1.0f, t / time));
+                Pump.transform.position = Vector3.Lerp(startPosition, transform.position, Mathf.SmoothStep(0.0f, 1.0f, t / time));
+            }
+        }
+    }
+}
